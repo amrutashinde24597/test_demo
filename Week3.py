07e9ce505a5ed7ed535d38a57e02bcb5da12b18c
Amruta <amruta@example.com>
2024-06-13 12:20:51 +0530

    add call to disk_full function

--- Week3.py
+++ Week3.py
@@ -3,12 +3,6 @@
 import os
 import sys
 
-
-def check_reboot():
-    """Returns True if the computer has a pending reboot."""
-    return os.path.exists("/run/reboot-required")
-
-
 def main():
     if check_reboot():
         print("Pending Reboot.")
